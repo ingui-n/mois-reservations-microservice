770e7d6ecc70d32e2eab7bb31ba438af752d279c
ingui-n <inguin@email.cz>
2025-10-26 22:12:03 +0100

    moved env composition of DATABSE_URL directly to database.ts

--- db/database.ts
+++ db/database.ts
@@ -1,3 +1,5 @@
 import {drizzle} from "drizzle-orm/node-postgres";
 
-export const db = drizzle(Bun.env.DATABSE_URL);
+export const db = drizzle(
+  `postgress://${Bun.env.RESERVATIONS_DATABASE_USER!}:${Bun.env.RESERVATIONS_DATABASE_PASSWORD!}@${Bun.env.DATABASE_IP}:5432/${Bun.env.RESERVATIONS_DATABASE_DB}`
+);
