df996c3cc8051831ea1c05c4a7a00134ca634a8d
ingui-n <inguin@email.cz>
2025-12-02 23:35:09 +0100

    an attempt to fix getReservations.js dates filters 2

--- routes/getReservations.js
+++ routes/getReservations.js
@@ -50,23 +50,10 @@ const getReservationsFromDatabase = async (headers, userId, computerId, from, to
         userId && eq(reservationsTable.userId, userId),
         computerId && eq(reservationsTable.computerId, computerId),
 
-        (from || to) ? or(
-          // Overlaps with 'from' (active at 'from' time)
-          from && and(
-            lte(reservationsTable.startDateTime, from),
-            gte(reservationsTable.endDateTime, from)
-          ),
-          // Overlaps with 'to' (active at 'to' time)
-          to && and(
-            lte(reservationsTable.startDateTime, to),
-            gte(reservationsTable.endDateTime, to)
-          ),
-          // Completely within [from, to]
-          from && to && and(
-            gte(reservationsTable.startDateTime, from),
-            lte(reservationsTable.endDateTime, to)
-          )
-        ) : undefined,
+        and(
+          from ? gte(reservationsTable.endDateTime, from) : undefined,
+          to ? lte(reservationsTable.startDateTime, to) : undefined
+        ),
 
         isNull(reservationsTable.deletedAt)
       )
